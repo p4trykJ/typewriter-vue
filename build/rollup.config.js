a54ae023168f7e03169e5c676ff7d1a17cae6bb1
Patryk Jezierski <wacool00@gmail.com>
2021-05-31 00:07:21 +0200

    build(rollup): fixed rollup transpiling and minification plugins

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,6 +1,7 @@
 import vue from "rollup-plugin-vue"; // Handle .vue SFC files
 import commonjs from "@rollup/plugin-commonjs"; // Convert CommonJS modules to ES6
 import { babel } from "@rollup/plugin-babel"; // Transpile/polyfill with reasonable browser support
+import { terser } from "rollup-plugin-terser"; // Minifies code
 
 export default {
   input: "./index.js", // Path relative to package.json
@@ -18,6 +19,8 @@ export default {
       exclude: "node_modules/**",
       include: "**/*.js",
       babelHelpers: "runtime",
+      extensions: [".js", ".jsx", ".es6", ".es", ".mjs", ".vue"],
     }),
+    ...(process.env.npm_config_minify ? [terser()] : []), // Minifies code only when --minify flag is set to true (currently only for iife format)
   ],
 };
